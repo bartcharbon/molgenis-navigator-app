59a7cd44915bd5105c4848bbf4a9c041cef9b11a
Morris Swertz <umcg-mswertz@Morriss-MacBook-Pro-2.local>
2017-01-22 18:38:51 +0100

    moved template from index.html to Navigator.vue

--- src/main.js
+++ src/main.js
@@ -1,28 +1,8 @@
 import Vue from 'vue'
-
-import getStateForPackage from './getStateForPackage.js'
-import getStateForSearch from './getStateForSearch.js'
-import state from './exampledata.js'
-
-//initialize state to root package (packageId = null)
-getStateForPackage(state, null)
+import Navigator from './Navigator.vue'
 
 //initalize view
 const app = new Vue({
   el: '#app',
-  methods: {
-    updateSearch: function() {
-      if (this.search != undefined) getStateForSearch(this, this.search)
-    },
-    clearSearch: function() {
-      getStateForPackage(this, null);
-    },
-    selectPackage: function(packageId) {
-      getStateForPackage(this, packageId);
-    },
-    openDataset: function(datasetId) {
-      window.open("/menu/main/dataexplorer?entity=" + datasetId);
-    }
-  },
-  data: state
+  render: h => h(Navigator)
 });
